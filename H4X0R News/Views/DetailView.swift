5d58d555847a526775d5933088ef622712b33589
Hitesh66 <hitesh.dayaramani66@gmail.com>
2023-08-09 20:13:38 +0530

    Hacker News

---
+++ H4X0R News/Views/DetailView.swift
@@ -0,0 +1,24 @@
+//
+//  DetailView.swift
+//  H4X0R News
+//
+//  Created by Hitesh Dayaramani on 7/24/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
+
